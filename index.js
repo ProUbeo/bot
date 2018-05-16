0c2841336218b01173abbb94c9d7960cc1a9db3c
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-05-16 19:55:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("sigbot!")
+var prefix = ("MagicBot!")
 
 bot.on('ready', function() {
-    bot.user.setUsername("Sigbot")
-    bot.user.setPresence({ game: { name: 'mentionner moi pour que je vous aide :3'}, status: 'online'}) //en ligne;
+    bot.user.setUsername("MagicBot")
+    bot.user.setPresence({ game: { name: 'brûler des mages malpoli'}, status: 'online'}) //en ligne;
     console.log("Connected")
 });
 
@@ -13,246 +13,137 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
-if (message.content === "sigbot!gif"){
-    message.channel.send("on ce met au image et au gif maintenant \n alors preparais vous car on a atein une nouvelle aire de jeux", {
-        file: "https://media.giphy.com/media/LOtqITm3tFmiA/giphy.gif" 
-    });  
+    if(message.content.startsWith("magie!feu")){
+        message.channel.send(`**${message.author.username}** lance la capacité de feu !\nDégâts : 25`,{
+        file: "https://orangemushroom.files.wordpress.com/2014/04/ignition-effect.gif"
+    })
 }
-if (message.content === "salut <@437179201284603904>"){
-    message.reply("Konichiwa :3");
-    console.log("une salutation a éter effectuer");
-}
-if (message.content === "ça va <@437179201284603904>"){
-        message.reply("JUST MONIKA");
-        file: "https://cdn.discordapp.com/attachments/427521450866507777/439488487306297364/monika.chr"
-}
-if (message.content === "merci <@437179201284603904>"){
-    message.reply("c'est toujours un plaisir :D");
-}
-if(message.content === "Xarrin!nickel"){ //Xarrin
-        message.channel.send("", {
-        file: "https://media1.tenor.com/images/996279409bcab56de0f7fce3d135a84c/tenor.gif"
-    }) 
-}
-if(message.content === "Xarrin!Invocation:Byakko"){ //Xarrin
-        message.channel.send("", {
-        file: "https://cdn.discordapp.com/attachments/434681503964725248/435060807118749700/SF_Byakko.png"
-    }) 
-}
-if(message.content === "Xarrin!voyeur"){ //Xarrin
-        message.channel.send("", {
-        file: "https://i.pinimg.com/originals/07/d3/ac/07d3ac2dcfc4597b4d0da463fa8b0003.gif"
-    }) 
-}
-if(message.content === "Xarrin!Invocation:Phoenix"){ //Xarrin
-    message.channel.send("", {
-    file: "https://cdn.discordapp.com/attachments/434681503964725248/439059526575063040/m035c10.png"
+if(message.content.startsWith("magie!tornade")){
+    message.channel.send(`**${message.author.username}** lance la capacité tornade!\nDégâts : 25`,{
+    file: "https://pa1.narvii.com/6394/6e5f4ce3b0f5ea1c28d9788aa2a6eb376a041314_hq.gif"
 })
 }
-if(message.content === "Lucas!KC"){ //MME POOF
-        message.channel.send("", {
-        file: "https://31.media.tumblr.com/b4bb500c7d05b0f44e0fd4c35020e3eb/tumblr_inline_ofauryqfpQ1s9x8us_500.gif"
-    }) 
-}
-if(message.content === "Lucas!FTW"){ //MME POOF
-        message.channel.send("Ma réponse :", {
-        file: "https://i.skyrock.net/0358/84470358/pics/3103693573_1_17_ENJacXlx.gif"
-    }) 
-}
-if(message.content === "Lucas!MrPuel"){ //MME POOF
-        message.channel.send("¡Hola!", {
-        file: "https://thumbs.gfycat.com/DentalHealthyAcornweevil-max-1mb.gif"
-    }) 
-}
-if(message.content === "yuki!invocation:Godness"){ //yuki
-        message.channel.send("", {
-        file: "https://cdn.discordapp.com/attachments/434681503964725248/435078318610710539/Goddess.png"
-    }) 
-}
-if(message.content === "Xarrin!gral"){ //xarin
-    message.channel.send("La solitude c'est quand karma il vas sur mer\nLa solitude c'est quand il est bourré dans un verre\n-----\nIl répond au nom de karma On dirais pas forcément mais il en a du karma\nNous on en peut plus de son karma\n-----\n La solitude c'est quand Lucas se prend un vent\nLa solitude c'est au moment ou il se prend du blanc\n-----\n Il répond au nom de lucas\nnon il ne mange pas encore de caca\nCar il était caché dans un placard\n -----\n La solitude c'est quand Xarrin  il mange son pain\nLa solitude c'est comme son fil il est tout fin\n-----\nil répond au nom de Xarrin\nXarrin, il aime allé touché des sein\nXarrin, il aime allé boire du Vin en touchant des popotins");
-}
-if(message.content === "Xarrin!invocation:spiritwater"){ //xarrin
-    message.channel.send("", {
-    file: "https://cdn.discordapp.com/attachments/434681503964725248/439071187570655232/elemental_01.png"
-}) 
-}
-if(message.content === "Xarrin!invocation:spiritearth"){ //xarin
-    message.channel.send("", {
-    file: "https://cdn.discordapp.com/attachments/434681503964725248/439071278964408330/elemental_02.png"
-}) 
-}
-if(message.content === "Xarrin!invocation:spiritwind"){ //xarin
-    message.channel.send("", {
-    file: "https://cdn.discordapp.com/attachments/434681503964725248/439071361919352842/elemental_03.png"
-}) 
-}
-if(message.content === "Xarrin!invocation:spiritfire"){ //xarin
-    message.channel.send("", {
-    file: "https://cdn.discordapp.com/attachments/434681503964725248/439071422992744448/elemental_04.png"
-}) 
-}
-if(message.content === "Xarrin!invocation:Kuro"){ //xarin
-    message.channel.send("", {
-    file: "https://cdn.discordapp.com/attachments/434681503964725248/439133776849797122/d825bf93ca36fdcbf62091b1cc4e6936.png"
+if(message.content.startsWith("magie!stick-magic")){
+    message.channel.send(`**${message.author.username}** lance la capacité stick magic!\nDégâts : 15`,{
+    file: "https://orangemushroom.files.wordpress.com/2012/06/magic-booster-effect.gif"
 })
 }
-if(message.content === "Xarrin!ham"){ //Xarrin
-    message.channel.send("", {
-    file: "https://pa1.narvii.com/5994/0561f42850b340049a2ed4628e1ffe358e0dbc96_hq.gif"
-}) 
-}
-if(message.content === "sakamoto!sleep"){ //Sakamoto-san
-    message.channel.send("", {
-    file: "https://media.giphy.com/media/bo0btBvaKDKeY/giphy.gif"
+if(message.content.startsWith("magie!nature")){
+    message.channel.send(`**${message.author.username}** lance la capacité orbe de la nature!\nDégâts : 25`,{
+    file: "https://i.pinimg.com/originals/69/eb/43/69eb43047ff86ade658b2c677c0567ee.gif"
 })
 }
-if(message.content === "sakamoto!play"){ //Sakamoto-san
-    message.channel.send("", {
-    file: "https://78.media.tumblr.com/fba185be4b118ac2bc9f2b913f02bc08/tumblr_n9yumpiQ3y1rmdq5eo4_500.gif"
+if(message.content.startsWith("magie!glace")){
+    message.channel.send(`**${message.author.username}** lance la capacité glace!\nDégâts : 20`,{
+    file: "https://blog-imgs-18-origin.fc2.com/e/t/e/eternalhydrogen/skill_21111005_effect.gif"
 })
 }
-if(message.content === "sakamoto!play 2"){ //Sakamoto-san
-    message.channel.send("", {
-    file: "https://media.giphy.com/media/LOtqITm3tFmiA/giphy.gif"
+if(message.content.startsWith("magie!éclair")){
+    message.channel.send(`**${message.author.username}** lance la capacité d'éléctricité!\nDégâts : 50`,{
+    file: "https://i.pinimg.com/originals/d7/f1/0e/d7f10ed0e12dce0a783373c2ae8b2c61.gif"
 })
 }
-if(message.content === "sakamoto!play 3"){ //Sakamoto-san
-    message.channel.send("", {
-    file: "https://i.gifer.com/8X8r.gif"
+if(message.content.startsWith("magie!vent")){ 
+    message.channel.send(`**${message.author.username}** lance la capacité vent!\nDégâts : 5`,{
+    file: "https://i.pinimg.com/originals/98/51/59/985159cc49c09556b29c2264208028ca.gif"
 })
 }
-if(message.content === "sakamoto!xD"){ //Sakamoto-san
-    message.channel.send("", {
-    file: "https://static.fjcdn.com/gifs/Sakamoto+anime+nichijou_80d9f8_4843458.gif"
+if(message.content.startsWith("magie!ice-axe")){ 
+    message.channel.send(`**${message.author.username}** lance la capacité hache magique!\nDégâts : 50\nSec : 10`,{
+    file: "https://2img.net/h/i1200.photobucket.com/albums/bb340/x3TheAran595/Chaos21110011effect.gif"
 })
 }
-if(message.content === "sakamoto!srx"){ //Sakamoto-san
-    message.channel.send("", {
-    file: "https://media.giphy.com/media/RRwhNPJMaymm4/giphy.gif"
+if(message.content.startsWith("magie!slash")){
+    message.channel.send(`**${message.author.username}** a utilisé compétence slach!\nDégâts : 15`,{
+    file: "https://i.pinimg.com/originals/9b/7f/62/9b7f623ff55be9b911b1f20c1f836250.gif"
 })
 }
-if(message.content === "I like trains"){ 
-    message.channel.send(":D", {
-    file: "https://i.imgur.com/7zkiG.gif"
+if(message.content.startsWith("magie!implosion")){
+    message.channel.send(`**${message.author.username}** lance la capacité implosion!\nDégâts : 50`,{
+    file: "https://conserver.files.wordpress.com/2010/03/09.gif"
 })
 }
-if(message.content === "salope Martine"){ 
-    message.channel.send("!!!!!", {
-    file: "https://cdn.discordapp.com/attachments/408257108665040896/444116108203786241/53f39401d3653f077c30c84c4acae7ac.png"
+if(message.content.startsWith("magie!protection")){
+    message.channel.send(`**${message.author.username}** lance la capacité protection!\nMur défendant de : 25 dégâts`,{
+    file: "https://orangemushroom.files.wordpress.com/2012/06/anti-magic-shell-effect.gif"
 })
 }
-if(message.content === "Qui est Mokigrokaka ?"){ 
-    message.channel.send("Cette salope ?", {
-    file: "https://cdn.discordapp.com/attachments/292726526657429504/444879710430822400/Mokigrokaka.PNG"
+if(message.content.startsWith("magie!onde")){
+    message.channel.send(`**${message.author.username}** lance la capacité onde!\nDégâts : 30`,{
+    file: "https://orangemushroom.files.wordpress.com/2011/11/energy-buster.gif"
 })
 }
-if (message.content === "<@437179201284603904>" ){ 
+if(message.content.startsWith("magie!soin")){
+    message.channel.send(`**${message.author.username}** lance la capacité de soins!\nSoigne : 10`,{
+    file: "https://i.pinimg.com/originals/2b/6e/9f/2b6e9f46c2cc37569eabe5c40731579c.gif"
+})
+}
+if(message.content.startsWith("magie!sword")){
+    message.channel.send(`**${message.author.username}** lance la capacité sword!\nDégâts : 75`,{
+    file: "https://orangemushroom.files.wordpress.com/2012/07/blaze-up-effect.gif"
+})
+}
+if(message.content.startsWith("magie!angel")){
+    message.channel.send(`**${message.author.username}** à utiliser la capacité pour avoir des ailes !`,{
+    file: "https://i.pinimg.com/originals/ce/0d/8d/ce0d8df5b15699d45caf2b32ef125380.gif"
+})
+}
+if(message.content.startsWith("magie!dragon")){
+    message.channel.send(`**${message.author.username}** lance le laser du dragon!\nDégâts : 75`,{
+    file: "https://2img.net/h/i1156.photobucket.com/albums/p575/x3TheAran5921/Kaiser65121100effect.gif"
+})
+}
+if (message.content === "magie!help 1" ){  
 
     const embed = new Discord.RichEmbed()
-    .setTitle("info sur Sigbot")
-    .setAuthor("Sig", "https://puyonexus.com/forum/download/file.php?avatar=1481_1311799010.png")
+    .setTitle("Magie niveau 1")
     .setColor(0x0086AE)
-    .setDescription("le bot a éter crée juste pour faire de la programation , mais est partie bien plus loin que sa, il a été crée par @Sig qui c'est fais aidez par  @̶̶ۣۣۜۜ͜ζ͜͡▪) ρнคгยxтαภ  pour les petite erreur :D")
-    .setFooter("pour plus d'info demander au créateur du bot :3", "https://orig00.deviantart.net/24f3/f/2015/305/4/e/sig_as_amitie__by_grimtales1lizzybird-d9f6ez9.png")
-    .setImage("https://78.media.tumblr.com/9a209d8b2d826c25b70b0fa8e29aeb0f/tumblr_n96mqjeBdg1qmbwk5o1_r2_500.png")
-    .setThumbnail("https://puyonexus.com/mediawiki/images/d/d9/Img201007.png")
-    .setTimestamp()
-    .setURL("https://discordapp.com/api/oauth2/authorize?client_id=437179201284603904&permissions=2146958583&scope=bot")
-    .addField("le bot peu reagir avec 3 maniere ",
-      "un texte \n le mentionner \n et utiliser le prefix\nutiliser la commande sigbot!help pour plus d'info")
-    .addField("prefix du bot", "le prefix du bot est sigbot! mais il est possible qu'il change", false);
+    .setDescription("Voici toute les attaque qu'un mage niveau 1 peu faire")
+    .setFooter("Si vous avez un probleme demander de l'aide a un modo ou au autre")
+    .addField("Commande magique",
+      "magie!feu (-25PV) (utilisable 3 foix)\nmagie!tornade (-25PV)\nmagie!stick-magic (-15PV) (utilisable 3 foix)\nmagie!nature (-25PV)\nmagie!glace (-20PV)")
+      .addField("Nombre de PV",
+      "100PV pour un mage niveau 1")
   
     message.channel.send({embed});
 }
-if (message.content === prefix + "help" ){  
+if (message.content === "magie!help" ){  
 
     const embed = new Discord.RichEmbed()
-    .setTitle("commande pour le Sigbot")
+    .setTitle("aide disponible ")
     .setColor(0x0086AE)
-    .setDescription("voici les commande que le bot peu faire :D")
-    .setFooter("pour plus d'info demander a son créateur :3")
-    .addField("commande qui devais etre drole",
-      "lui dire salut (avec une mention a la fin) \n lui demander si ça va (avec une mention a la fin) \n lui dire merci (avec une mention a la fin)")
-      .addField("commande special",
-      "sigbot!gif")
+    .setDescription("magie!help 1\nmagie!help 2\nmagie!help 3")
+    .setFooter("Si vous avez un probleme demander de l'aide a un modo ou au autre")
   
     message.channel.send({embed});
 }
-if(message.content === "mp!help"){ //MME POOF HELP
+if (message.content === "magie!help 2"){  
 
     const embed = new Discord.RichEmbed()
-    .setTitle("commande pour les gif")
+    .setTitle("Magie niveau 2")
     .setColor(0x0086AE)
-    .setDescription("voila les gif et image que Madame Pouf posséde")
-    .setThumbnail("https://cdn.discordapp.com/attachments/432275341202030614/435096117504638986/unknown.png")
-    .setFooter("pour plus d'info demander a sig :D")
-    .addField("gif",
-      "Lucas!KC\nLucas!FTW\Lucas!MrPuel\nLucas!MrPuel")
-      .addField("image",
-      "rien") 
+    .setDescription("Voici toute les attaque qu'un mage niveau 2 peu faire")
+    .setFooter("Si vous avez un probleme demander de l'aide a un modo ou au autre")
+    .addField("Commande magique",
+      "magie!eclair (-50PV) (utilisable 3 fois par match)\nmagie!vent (-5) (cumulable avec une autre attaque pour ajouter 5 dégâts voir plus)\nmagie!ice-axe : normal : 50 de dégâts. et à sec par une attaque magie!feu: 10 de dégâts \nmagie!slach (-15PV)\nmagie!implosion (-50PV) (utilisable 1 fois par combats)\nmagie!protection : défend de 25 de dégâts (mais si l'attaque fait plus tu es obligé de te faire toucher) (utilisable 3 fois par combats)")
+      .addField("Nombre de PV",
+      "200PV pour un mage niveau 2")
 
     message.channel.send({embed});
 }
-if(message.content === "yuki!help"){ //yuki help
+if (message.content === "magie!help 3"){  
 
     const embed = new Discord.RichEmbed()
-    .setTitle("commande pour les gif")
+    .setTitle("Magie niveau 2")
     .setColor(0x0086AE)
-    .setDescription("voila les gif et image que Yuki posséde")
-    .setThumbnail("https://cdn.discordapp.com/attachments/434681503964725248/435083973069373441/ange_manga_n1.jpg")
-    .setFooter("pour plus d'info demander a sig :D")
-    .addField("gif",
-      "rien")
-    .addField("image",
-      "yuki!invocation:Godness")
-
-    message.channel.send({embed});
-}
-if(message.content === "sig!help"){ //sig help
-
-    const embed = new Discord.RichEmbed()
-    .setTitle("commande pour les gif")
-    .setColor(0x0086AE)
-    .setDescription("voila les gif et image que sig posséde")
-    .setThumbnail("https://image.gamer.ne.jp/news/2011/20110727/00018cb052ae8768e94585c1314982a7a0cc/o/17.jpg")
-    .setFooter("pour plus d'info va te voir vue que c'est toi qui la crée xD")
-    .addField("gif",
-      "sig!win \nsig!lose\nsig!magic 1\nsig!magic 2\nsig!magic 3\n sig!magic 4")
-      .addField("image",
-      "aucun")
-
-    message.channel.send({embed});
-}
-if(message.content === "Xarrin!help"){ //Xarrin help
- 
-    const embed = new Discord.RichEmbed()
-    .setTitle("commande pour les gif")
-    .setColor(0x0086AE)
-    .setDescription("voila les gif et image que Xarrin posséde")
-    .setThumbnail("https://cdn.discordapp.com/attachments/434681503964725248/435060807118749700/SF_Byakko.png")
-    .setFooter("pour plus d'info demander a sig :D")
-    .addField("gif",
-      "Xarrin!nickel\nXarrin!voyeur\nXarrin!ham")
-      .addField("image",
-      "Xarrin!Invocation:Byakko\nXarrin!Invocation:Phoenix\nXarrin!invocation:spiritwater\nXarrin!invocation:spiritearth\nXarrin!invocation:spiritwind\nXarrin!invocation:spiritfire")
-
-    message.channel.send({embed});
-}
-if(message.content === "sakamoto!help"){ //sakamoto help
- 
-    const embed = new Discord.RichEmbed()
-    .setTitle("commande pour les gif")
-    .setColor(0x0086AE)
-    .setDescription("voila les gif et image que Sakamoto-san posséde")
-    .setThumbnail("http://i0.kym-cdn.com/photos/images/newsfeed/000/714/836/f31.gif")
-    .setFooter("pour plus d'info demander a sig :D")
-    .addField("gif",
-      "sakamoto!sleep\nsakamoto!play\nsakamoto!play 2\nsakamoto!play 3\nsakamoto!xD\nsakamoto!srx")
-      .addField("image",
-      "aucun")
+    .setDescription("Voici toute les attaque qu'un mage niveau 3 peu faire")
+    .setFooter("Si vous avez un probleme demander de l'aide a un modo ou au autre")
+    .addField("Commande magique",
+      "magie!soin : soigne de 10 pv (à utiliser 3 fois par match)\nmagie!onde : fait 30 de dégâts\nmagie!sword : fait 75 de dégâts (à utiliser 3 fois par match)\nmagie!angel : fait s'envoler dans les airs pour esquiver un attaque qui se passe au sol ou qui est pas redirigable !\nmagie!dragon : fait 75 de dégâts (utilisable 3 fois par match)")
+      .addField("Nombre de PV",
+      "300PV pour un mage niveau 3")
 
     message.channel.send({embed});
 }
 }
-) 
+)
